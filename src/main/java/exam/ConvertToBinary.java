e1b256b6f7df59c98e2938e8824bc3dbb2cd6357
Rutuja-Tukaram-Pangavhane <86228578+Rutuja-Tukaram-Pangavhane@users.noreply.github.com>
2021-06-22 12:27:31 +0530

    Update ConvertToBinary.java

--- src/main/java/exam/ConvertToBinary.java
+++ src/main/java/exam/ConvertToBinary.java
@@ -1,9 +1,53 @@
 package exam;
 
-public class ConvertToBinary {
+public class ConvertToBinary
+{
 
-  public String toBinary(final int n) {
-    //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-    return "";
+  public String toBinary(final int n,String s)
+  {
+      
+     
+        for (int i = 0; i < n; i++) 
+        {
+            // convert each char to
+            // ASCII value
+            int val = Integer.valueOf(s.charAt(i));
+  
+            // Convert ASCII value to binary
+            String bin = "";
+            while (val > 0) 
+            {
+                if (val % 2 == 1)
+                {
+                    bin += '1';
+                }
+                else
+                    bin += '0';
+                val /= 2;
+      
+                bin = reverse(bin);
+  
+            System.out.print(bin + " ");
+            }
+        }
   }
-}
+  
+    static String reverse(String input) 
+    {
+        char[] a = input.toCharArray();
+        int l, r = 0;
+        r = a.length - 1;
+  
+        for (l = 0; l < r; l++, r--)
+        {
+            // Swap values of l and r 
+            char temp = a[l];
+            a[l] = a[r];
+            a[r] = temp;
+        }
+        return String.valueOf(a);
+    }
+      
+     
+  
+ }
